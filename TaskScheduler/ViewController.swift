a72338b0f00bcc6be9979f858d528031f4cfd319
llauderesv <llauderesv@gmail.com>
2017-02-06 12:35:06 +0800

    Initial Commit

---
+++ TaskScheduler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaskScheduler
+//
+//  Created by Orange Apps on 06/02/2017.
+//  Copyright © 2017 Orange Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
